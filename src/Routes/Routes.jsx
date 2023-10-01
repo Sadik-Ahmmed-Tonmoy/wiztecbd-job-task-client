48ddf0e82b6185093a02e72a8228082096494230
Sadik-Ahmmed-Tonmoy <100148160+Sadik-Ahmmed-Tonmoy@users.noreply.github.com>
2023-10-01 20:56:29 +0600

    initial commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Layout/Main/Main";
+import Home from "../Pages/Home/Home/Home";
+
+  
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+    //   errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home/>,
+        },
+      ],
+    },
+  ]);
